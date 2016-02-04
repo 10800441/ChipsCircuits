c5a204bd858e1eaf8642b5b22e4f98fd5c635d0d
mwvanham <mwvanham@gmail.com>
2016-02-04 20:03:11 +0100

    Hiephoi3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println(" vandaag at ik een pinda-");
         System.out.println(" vandaag at ik een pinda-");
+        System.out.println(" vandaag at ik een pinda-");
+
 
     }
 }
