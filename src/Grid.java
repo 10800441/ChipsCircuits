0a893d919d7f39600724679b219f752cc1418ec4
10800441 <martyn_heijstek@hotmail.com>
2016-03-09 09:19:20 +0100

    astar heeft nu een score functie (beetje omslachtig maar bijslijpen moet nog gebeuren)

--- src/Grid.java
+++ src/Grid.java
@@ -37,10 +37,8 @@ public class Grid {
 
         for(int i = 0; i < miniQueue.size(); i++){
 
-             ExpandGrid alpha = (ExpandGrid) miniQueue.get(i);
-            alpha.grid.printGrid();
-            System.out.println("estimate " + alpha.estimate);
-            System.out.println("steps " + alpha.steps);
+            ExpandGrid alpha = (ExpandGrid) miniQueue.get(i);
+
         }
         return miniQueue;
 
@@ -202,12 +200,9 @@ public class Grid {
         int gateNumber = netGate.gate2;
         int y2 = gateDatabase[gateNumber][1];
         int x2 = gateDatabase[gateNumber][2];
+        int z2 = 0;
 
-
-
-
-
-        return Math.abs(x2-x) + Math.abs(y2-y); // +  Math.abs(z2-z);
+        return Math.abs(x2-x) + Math.abs(y2-y) + Math.abs(z2-z);
     }
 
 }
