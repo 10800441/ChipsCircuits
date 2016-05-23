856ec22ca2ce8db8892dfaf235150857814b7c13
Michelle <appel.michelle@gmail.com>
2016-05-23 11:51:17 +0200

    hehe

--- src/solver/Main.java
+++ src/solver/Main.java
@@ -1,8 +1,6 @@
 package solver;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.PriorityQueue;
